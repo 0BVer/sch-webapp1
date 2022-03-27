687ff3797371d60391f6f3990b9fea124f367814
0BVer <dr0joon@gmail.com>
2022-03-27 16:36:13 +0900

    CHORE : change line format

--- src/main/java/com/example/webapp1/HelloServlet.java
+++ src/main/java/com/example/webapp1/HelloServlet.java
@@ -6,9 +6,7 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
+    private String message;public void init() {
         message = "Hello World!";
     }
 
